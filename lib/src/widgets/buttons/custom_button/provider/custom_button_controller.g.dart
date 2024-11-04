13b90da1d82bd9df5725da14159c4398f1c13fb2
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-04 22:20:56 +1300

    feature(widget): Button and their respective models were implemented

---
+++ lib/src/widgets/buttons/custom_button/provider/custom_button_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'custom_button_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$customButtonControllerHash() =>
+    r'b1f7eb2ce593234b32ce6ef00ab914e80ea65467';
+
+/// See also [CustomButtonController].
+@ProviderFor(CustomButtonController)
+final customButtonControllerProvider = AutoDisposeAsyncNotifierProvider<
+    CustomButtonController, CustomButtonState>.internal(
+  CustomButtonController.new,
+  name: r'customButtonControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$customButtonControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CustomButtonController = AutoDisposeAsyncNotifier<CustomButtonState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
