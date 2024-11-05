b75e08ae6123fac189597140ad5b59e7de1c4f20
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-05 15:43:34 +1300

    feature(widget): implemented input

--- lib/src/widgets/index_widget.dart
+++ lib/src/widgets/index_widget.dart
@@ -1,5 +1,7 @@
 import 'package:forms360_uikit/src/widgets/buttons/custom_button/custom_button.dart';
+import 'package:forms360_uikit/src/widgets/inputs/input_index.dart';
 
  class UiWidget {
+  Inputs inputs = Inputs();
   UiButton buttons = UiButton();
 }
