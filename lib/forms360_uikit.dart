d86159ad844ec60c2c1779d625f657d8000809b5
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-05 15:50:25 +1300

    feature(widget): implemented input

--- lib/forms360_uikit.dart
+++ lib/forms360_uikit.dart
@@ -2,6 +2,7 @@ import 'package:forms360_uikit/src/util/index_util.dart';
 import 'package:forms360_uikit/src/widgets/index_widget.dart';
 
 //models
+export 'package:forms360_uikit/src/model/input_types.dart';
 export 'package:forms360_uikit/src/model/button_type.dart';
 export 'package:forms360_uikit/src/model/font_family_type.dart';
 
