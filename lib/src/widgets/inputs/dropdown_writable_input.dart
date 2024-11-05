a5cb63263eda9c80f1c102f4b22c4b71308602fd
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-05 22:51:48 +1300

    feature(widget): implemented dropdown

---
+++ lib/src/widgets/inputs/dropdown_writable_input.dart
@@ -0,0 +1,174 @@
+import 'package:drop_down_search_field/drop_down_search_field.dart';
+import 'package:flutter/material.dart';
+import 'package:forms360_uikit/forms360_uikit.dart';
+import 'package:forms360_uikit/src/util/texts.dart';
+
+enum DropdownWritableInputType {
+  MULTI,
+  SINGLE,
+}
+
+class DropdownWritableInput extends StatefulWidget {
+  DropdownWritableInput({
+    super.key,
+    required this.items,
+    required this.label,
+    required this.hintText,
+    required this.inputColor,
+    this.onSelectedValuesChanged,
+    this.selectedValues = const [],
+    required this.onSuggestionSelected,
+    required this.dropdownSearchFieldController,
+    this.type = DropdownWritableInputType.SINGLE,
+  });
+
+  final String label;
+  final String hintText;
+  final List<String> items;
+  final List<String> selectedValues;
+  final DropdownWritableInputType type;
+  final PrimaryInputColorKit inputColor;
+  final void Function(String) onSuggestionSelected;
+  final Function(List<String>)? onSelectedValuesChanged;
+  final TextEditingController dropdownSearchFieldController;
+
+  @override
+  State<DropdownWritableInput> createState() => _DropdownWritableInputState();
+}
+
+class _DropdownWritableInputState extends State<DropdownWritableInput> {
+  String? _selectedValue;
+
+  SuggestionsBoxController suggestionBoxController = SuggestionsBoxController();
+
+  List<String> getSuggestions(String query) {
+    List<String> matches = <String>[];
+    matches.addAll(widget.items);
+
+    matches.retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
+    return matches;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        DropDownSearchFormField(
+          textFieldConfiguration: TextFieldConfiguration(
+            style: FormsTextStyle().primary.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: _generateColor(),
+                ),
+            cursorColor: _generateColor(),
+            decoration: InputDecoration(
+              hintText: widget.hintText,
+              labelText: widget.label,
+              labelStyle: FormsTextStyle().primary.copyWith(
+                    fontSize: 16,
+                    color: _generateColor(),
+                  ),
+              hintStyle: FormsTextStyle().primary.copyWith(
+                    fontSize: 16,
+                    color: _generateColor(),
+                  ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: _generateColor(),
+                ),
+              ),
+              disabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: _generateColor(),
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: _generateColor(),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: _generateColor(),
+                ),
+              ),
+            ),
+            controller: widget.dropdownSearchFieldController,
+          ),
+          suggestionsCallback: (pattern) {
+            return getSuggestions(pattern);
+          },
+          itemBuilder: (context, String suggestion) {
+            return ListTile(
+              title: Text(suggestion),
+            );
+          },
+          itemSeparatorBuilder: (context, index) {
+            return const Divider();
+          },
+          transitionBuilder: (context, suggestionsBox, controller) {
+            return suggestionsBox;
+          },
+          onSuggestionSelected: (String suggestion) {
+            if (widget.type == DropdownWritableInputType.MULTI) {
+              if (widget.selectedValues.contains(suggestion)) {
+                widget.selectedValues.remove(suggestion);
+              } else {
+                widget.selectedValues.add(suggestion);
+              }
+
+              setState(() {});
+              if (widget.onSelectedValuesChanged != null) {
+                widget.onSelectedValuesChanged!(widget.selectedValues);
+              }
+            } else {
+              widget.dropdownSearchFieldController.text = suggestion;
+            }
+          },
+          suggestionsBoxController: suggestionBoxController,
+          validator: (value) {
+            if (value!.isEmpty &&
+                widget.type == DropdownWritableInputType.SINGLE) {
+              return 'Please select a value';
+            } else {
+              return null;
+            }
+          },
+          onSaved: (value) => widget.type == DropdownWritableInputType.SINGLE
+              ? _selectedValue = value
+              : widget.selectedValues.add(value!),
+          displayAllSuggestionWhenTap: true,
+        ),
+        if (widget.selectedValues.isNotEmpty)
+          Wrap(
+            spacing: 5.0,
+            children: List<Widget>.generate(
+              widget.selectedValues.length,
+              (int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Chip(
+                    label: Text(widget.selectedValues[index]),
+                    onDeleted: () {
+                      widget.selectedValues
+                          .remove(widget.selectedValues[index]);
+                      setState(() {});
+                      if (widget.onSelectedValuesChanged != null) {
+                        widget.onSelectedValuesChanged!(widget.selectedValues);
+                      }
+                    },
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+      ],
+    );
+  }
+
+  Color _generateColor() {
+    if (widget.inputColor == PrimaryInputColorKit.BLACK) return Colors.black;
+    return Theme.of(context).colorScheme.primary;
+  }
+}
--- lib/src/widgets/inputs/input_index.dart
+++ lib/src/widgets/inputs/input_index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:forms360_uikit/src/model/input_types.dart';
+import 'package:forms360_uikit/src/widgets/inputs/dropdown_writable_input.dart';
 import 'package:forms360_uikit/src/widgets/inputs/primary_input.dart';
 
 class Inputs {
@@ -22,5 +23,32 @@ class Inputs {
         isPassword: isPassword,
         inputColor: inputColor,
         controller: controller,
+      );
+
+  Widget dropdownWritableInput({
+    bool enabled = true,
+    required String label,
+    bool isPassword = false,
+    required String hintText,
+    required List<String> items,
+    void Function(String)? onChanged,
+    List<String> selectedValues = const [],
+    final String? Function(String?)? validator,
+    required Function(String) onSuggestionSelected,
+    Function(List<String>)? onSelectedValuesChanged,
+    required TextEditingController dropdownSearchFieldController,
+    PrimaryInputColorKit inputColor = PrimaryInputColorKit.BLACK,
+    DropdownWritableInputType type = DropdownWritableInputType.SINGLE,
+  }) =>
+      DropdownWritableInput(
+        type: type,
+        label: label,
+        items: items,
+        hintText: hintText,
+        inputColor: inputColor,
+        selectedValues: selectedValues,
+        onSuggestionSelected: onSuggestionSelected,
+        onSelectedValuesChanged: onSelectedValuesChanged,
+        dropdownSearchFieldController: dropdownSearchFieldController,
       );
 }
