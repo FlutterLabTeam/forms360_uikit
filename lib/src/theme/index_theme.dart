66ac34795faa228c72dc205dd5870f088d7e5584
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-06 19:34:45 +1300

    feature(theme): implemented text

--- lib/src/theme/index_theme.dart
+++ lib/src/theme/index_theme.dart
@@ -1,6 +1,7 @@
+import 'package:forms360_uikit/forms360_uikit.dart';
 import 'package:forms360_uikit/src/theme/colors/index_color.dart';
 
 class Theme {
   ThemeColor color = ThemeColor();
-  ThemeColor text = ThemeColor();
+  AppearanceKitTextTheme text = AppearanceKitTextTheme.build();
 }
--- lib/src/theme/text/index_text.dart
+++
@@ -1,3 +0,0 @@
-import 'package:forms360_uikit/src/theme/text/texts.dart';
-
-AppearanceKitTextTheme text = AppearanceKitTextTheme.build();
