74f9ab0ec80273dd4489c4d30ecf2d136c70d4b5
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-06 19:39:39 +1300

    feature(theme): implemented text

--- lib/forms360_uikit.dart
+++ lib/forms360_uikit.dart
@@ -5,11 +5,15 @@ import 'package:forms360_uikit/src/widgets/index_widget.dart';
 export 'package:forms360_uikit/src/model/input_types.dart';
 export 'package:forms360_uikit/src/model/button_type.dart';
 export 'package:forms360_uikit/src/model/font_family_type.dart';
+export 'package:forms360_uikit/src/model/theme_models/theme_data.dart';
+export 'package:forms360_uikit/src/model/theme_models/theme_data_factory.dart';
 
 // theme
 export 'package:forms360_uikit/src/theme/text/texts.dart';
 export 'package:forms360_uikit/src/theme/colors/colors.dart';
 export 'package:forms360_uikit/src/theme/universal_theme_factory/universal_theme.dart';
+
+
 
 
 //abstract class
