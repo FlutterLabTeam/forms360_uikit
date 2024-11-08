d40b95b269724e6353b7c602bf4af9716a401479
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-08 17:58:26 +1300

    feature(theme): implemented isBig input

--- lib/src/widgets/buttons/custom_button/custom_button.dart
+++ lib/src/widgets/buttons/custom_button/custom_button.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forms360_uikit/src/theme/text/texts.dart';
 import 'package:forms360_uikit/src/model/button_type.dart';
-import 'package:forms360_uikit/src/widgets/async_value_widget.dart';
 import 'package:forms360_uikit/src/widgets/buttons/custom_button/widget/buttons.dart';
-import 'package:forms360_uikit/src/widgets/buttons/custom_button/provider/custom_button_controller.dart';
 
 class UiButton {
   Widget button({
@@ -23,7 +20,108 @@ class UiButton {
       );
 }
 
-class _CustomButton extends ConsumerWidget {
+class _CustomButton extends StatefulWidget {
+  final String title;
+  final Function() onPressed;
+  final ButtonTypeKit buttonType;
+  final SizeButtonKit sizeButton;
+  final ButtonStateKit buttonState;
+
+  const _CustomButton({
+    required this.title,
+    required this.onPressed,
+    required this.sizeButton,
+    required this.buttonType,
+    required this.buttonState,
+  });
+
+  @override
+  State<_CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<_CustomButton> {
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () async {
+        if (widget.buttonState != ButtonStateKit.disabled && isLoading) {
+          onTap(widget.onPressed());
+        }
+      },
+      child: Container(
+        height: _generateSize(widget.sizeButton),
+        decoration: BoxDecoration(
+          color: _buttonColor(context),
+          border: _borderColor(context),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: isLoading
+            ? Center(child: ButtonLoading())
+            : Center(
+                child: Text(widget.title, style: _textButtonColor(context)),
+              ),
+      ),
+    );
+  }
+
+  BoxBorder? _borderColor(BuildContext context) {
+    Color color = Theme.of(context).colorScheme.primary;
+
+    if (widget.buttonType == ButtonTypeKit.primary) return null;
+
+    if (widget.buttonType == ButtonTypeKit.tertiary) {
+      color = Colors.white;
+    }
+    if (widget.buttonState == ButtonStateKit.disabled) {
+      color = Color(0xFFC9DEEE);
+    }
+
+    return Border.all(width: 2, color: color);
+  }
+
+  TextStyle _textButtonColor(BuildContext context) {
+    Color textColor = Colors.white;
+
+    if (widget.buttonType == ButtonTypeKit.secondary) {
+      textColor = Theme.of(context).colorScheme.primary;
+    }
+    if (widget.buttonState == ButtonStateKit.disabled) {
+      textColor = Theme.of(context).colorScheme.primary;
+    }
+
+    return AppearanceKitTextTheme.build().button.copyWith(color: textColor);
+  }
+
+  Color _buttonColor(BuildContext context) {
+    switch (widget.buttonType) {
+      case ButtonTypeKit.primary:
+        return Theme.of(context).colorScheme.primary;
+      case ButtonTypeKit.secondary:
+        return Colors.white;
+      case ButtonTypeKit.tertiary:
+        return Colors.transparent;
+    }
+  }
+
+  double _generateSize(SizeButtonKit size) {
+    switch (size) {
+      case SizeButtonKit.big:
+        return 60;
+      case SizeButtonKit.small:
+        return 40;
+    }
+  }
+
+  onTap(Function() callback, ) async {
+    setState(() => isLoading = true);
+    await callback();
+    setState(() => isLoading = false);
+  }
+}
+
+/*class _CustomButton extends ConsumerWidget {
   final String title;
   final Function() onPressed;
   final ButtonTypeKit buttonType;
@@ -114,4 +212,4 @@ class _CustomButton extends ConsumerWidget {
         return 40;
     }
   }
-}
+}*/
