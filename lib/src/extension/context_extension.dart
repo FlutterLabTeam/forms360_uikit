bbee08c92f2dab556d129ece02ed63c5094f83c2
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-27 10:21:33 +1300

    feature(theme): implemented color theme

--- lib/src/extension/context_extension.dart
+++ lib/src/extension/context_extension.dart
@@ -38,39 +38,32 @@ extension ContextExtension on BuildContext {
       );
 
   //colors them access by context
+
+  Color get grey1 => AppearanceKitColors.light().grey1; //red
+  Color get grey2 => AppearanceKitColors.light().grey2; //red
+  Color get grey3 => AppearanceKitColors.light().grey3; //red
+
+
   Color get errorColor => Theme.of(this).colorScheme.error; //red
   Color get primaryColor => Theme.of(this).colorScheme.primary; //blue
   Color get surfaceColor => Theme.of(this).colorScheme.surface; //white
   Color get secondaryColor => Theme.of(this).colorScheme.secondary; // green
-  Color get onPrimaryColor => Theme.of(this).colorScheme.onPrimary;
-
-  ///dark blue
-  Color get onSurfaceColor =>
-      Theme.of(this).colorScheme.onSurface; //blue sea water
-  Color get surfaceContainerColor =>
-      Theme.of(this).colorScheme.surfaceContainer; //light blue
+  Color get onPrimaryColor => Theme.of(this).colorScheme.onPrimary;///dark blue
+  Color get onSurfaceColor => Theme.of(this).colorScheme.onSurface; //blue sea water
+  Color get surfaceContainerColor => Theme.of(this).colorScheme.surfaceContainer; //light blue
 
 //colors them access by context
-  TextStyle get titleText =>
-      AppearanceKitTextTheme.build().title.copyWith(color: onPrimaryColor);
+  TextStyle get titleText => AppearanceKitTextTheme.build().title.copyWith(color: onPrimaryColor);
 
-  TextStyle get buttonText =>
-      AppearanceKitTextTheme.build().button.copyWith(color: onPrimaryColor);
+  TextStyle get buttonText => AppearanceKitTextTheme.build().button.copyWith(color: onPrimaryColor);
 
-  TextStyle get inputText =>
-      AppearanceKitTextTheme.build().input.copyWith(color: onPrimaryColor);
+  TextStyle get inputText => AppearanceKitTextTheme.build().input.copyWith(color: onPrimaryColor);
 
-  TextStyle get primaryText =>
-      AppearanceKitTextTheme.build().primary.copyWith(color: onPrimaryColor);
+  TextStyle get primaryText => AppearanceKitTextTheme.build().primary.copyWith(color: onPrimaryColor);
 
-  TextStyle get descriptionText => AppearanceKitTextTheme.build()
-      .description
-      .copyWith(color: onPrimaryColor);
+  TextStyle get descriptionText => AppearanceKitTextTheme.build().description.copyWith(color: onPrimaryColor);
 
-  TextStyle get secondaryText =>
-      AppearanceKitTextTheme.build().secondary.copyWith(color: onPrimaryColor);
+  TextStyle get secondaryText => AppearanceKitTextTheme.build().secondary.copyWith(color: onPrimaryColor);
 
-  TextStyle get smallDescriptionText => AppearanceKitTextTheme.build()
-      .smallDescription
-      .copyWith(color: onPrimaryColor);
+  TextStyle get smallDescriptionText => AppearanceKitTextTheme.build().smallDescription.copyWith(color: onPrimaryColor);
 }
