c645362f5950bfc511c817e25a30fba2c73ca0a6
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-11-28 18:37:14 +1300

    feature(widget): implemented MediumScreenWidget changed

--- lib/forms360_uikit.dart
+++ lib/forms360_uikit.dart
@@ -5,6 +5,7 @@ import 'package:forms360_uikit/src/widgets/index_widget.dart';
 export 'package:forms360_uikit/src/model/input_types.dart';
 export 'package:forms360_uikit/src/model/button_type.dart';
 export 'package:forms360_uikit/src/model/pop_up_model.dart';
+export 'package:forms360_uikit/src/model/permission_type.dart';
 export 'package:forms360_uikit/src/model/font_family_type.dart';
 export 'package:forms360_uikit/src/model/menu_item_type_type.dart';
 export 'package:forms360_uikit/src/model/theme_models/theme_data.dart';
@@ -17,6 +18,7 @@ export 'package:forms360_uikit/src/theme/universal_theme_factory/universal_theme
 
 //extension
 export 'package:forms360_uikit/src/extension/context_extension.dart';
+export 'package:forms360_uikit/src/extension/permission_extension.dart';
 export 'package:forms360_uikit/src/extension/font_family_extension.dart';
 
 
