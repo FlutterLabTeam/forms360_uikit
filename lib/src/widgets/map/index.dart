fe1e0abdc1cdc65e1b8a50a1ed9f882c67b009e3
virgiliohierroii <132996070+virgiliohierroii@users.noreply.github.com>
2025-01-19 12:04:22 -0500

    Update index.dart

--- lib/src/widgets/map/index.dart
+++ lib/src/widgets/map/index.dart
@@ -1,1 +1,18 @@
-class Map {}
+import 'package:forms360_uikit/src/widgets/map/map_widget.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Maps {
+  map({
+    required Function(Map<MarkerId, Marker>) onSave,
+    required LatLng initialLocation,
+    bool hasMultipleMarkers = false,
+    bool showControls = true,
+  }) {
+    return MapWidget(
+      initialLocation,
+      onSave: onSave,
+      hasMultipleMarkers: hasMultipleMarkers,
+      showControls: showControls,
+    );
+  }
+}
