8232461b7a372fdc9d3b640d22d4a6f9df9e09c5
virgiliohierroii <132996070+virgiliohierroii@users.noreply.github.com>
2025-02-15 14:02:55 -0500

    Update tabbed_pop_up.dart

--- lib/src/widgets/pop_up/tabbed_pop_up.dart
+++ lib/src/widgets/pop_up/tabbed_pop_up.dart
@@ -137,7 +137,7 @@ class TabbedWidgetState extends State<TabbedWidget> {
   Container buildNavigatorUi(BuildContext context) {
     return Container(
       height: 50,
-      margin: EdgeInsets.only(top: 20, left: 20),
+      margin: EdgeInsets.only(top: 20, left: 20, right: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
