083c3e47b56b2efe29e6c477cbd51d670e45b67b
jeisonmanuelvargasvargas <jeisonvargas@flutterlabapps.com>
2025-03-18 13:18:01 -0500

    feature(gridView): implementing grid_view_generator.dart

--- lib/src/widgets/list/grid_view_generator.dart
+++ lib/src/widgets/list/grid_view_generator.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:forms360_uikit/forms360_uikit.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class GridViewGenerator<T> extends StatelessWidget {
@@ -23,12 +24,34 @@ class GridViewGenerator<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MasonryGridView.count(
-      itemCount: list.length,
-      crossAxisCount: crossAxisCount ?? 3,
-      mainAxisSpacing: mainAxisSpacing ?? 16,
-      crossAxisSpacing: crossAxisSpacing ?? 30,
-      itemBuilder: (_, int index) => itemBuilder(list[index], index),
-    );
+    return list.isNotEmpty
+        ? MasonryGridView.count(
+            itemCount: list.length,
+            crossAxisCount: crossAxisCount ?? 3,
+            mainAxisSpacing: mainAxisSpacing ?? 16,
+            crossAxisSpacing: crossAxisSpacing ?? 30,
+            itemBuilder: (_, int index) => itemBuilder(list[index], index),
+          )
+        : Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  icon ?? Icons.inbox,
+                  size: 60,
+                  color: context.primaryColor,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  label ?? "No matching records found",
+                  style: FormsKit.theme.text.primary.copyWith(
+                    color: context.primaryColor,
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
