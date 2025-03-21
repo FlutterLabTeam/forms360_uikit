ca49fadf0a36f4d2981dd66c05bb73eadd0beda2
jeisonmanuelvargasvargas <jeisonvargas@flutterlabapps.com>
2025-03-21 12:12:43 -0500

    feature(menu): menuIcon

--- lib/src/extension/widget_extension.dart
+++ lib/src/extension/widget_extension.dart
@@ -1,8 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:forms360_uikit/forms360_uikit.dart';
 
 extension WidgetExtension on Widget {
-  Widget get cursorGesture => MouseRegion(
-        child: this,
-        cursor: SystemMouseCursors.click,
-      );
+  Widget get cursorGesture {
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        bool isHovered = false;
+        return MouseRegion(
+          onEnter: (_) => setState(() => isHovered = true),
+          onExit: (_) => setState(() => isHovered = false),
+          onHover: (_) => setState(() => isHovered = true),
+          cursor: SystemMouseCursors.click,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              color: isHovered
+                  ? context.primaryColor.withOpacity(0.5)
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: this,
+          ),
+        );
+      },
+    );
+  }
 }
