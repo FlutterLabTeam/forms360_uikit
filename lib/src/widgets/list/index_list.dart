d526628da457a0b44bd12ba121a373712ecac785
jeisonmanuelvargasvargas <jeisonvargas@flutterlabapps.com>
2025-03-26 18:51:29 -0500

    added WrapGenerator

--- lib/src/widgets/list/index_list.dart
+++ lib/src/widgets/list/index_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:forms360_uikit/src/widgets/list/grid_view_generator.dart';
 import 'package:forms360_uikit/src/widgets/list/list_generator.dart';
+import 'package:forms360_uikit/src/widgets/list/wrap_generate.dart';
 
 class UiList {
   ListGenerator listGenerator<T>({
@@ -35,3 +36,22 @@ class UiList {
         crossAxisSpacing: crossAxisSpacing,
       );
 }
+
+WrapGenerator wrapGenerator<T>({
+  String? label,
+  IconData? icon,
+  int? crossAxisCount,
+  required List<T> list,
+  double? mainAxisSpacing,
+  double? crossAxisSpacing,
+  required Function(T, int) itemBuilder,
+}) =>
+    WrapGenerator<T>(
+      list: list,
+      icon: icon,
+      label: label,
+      itemBuilder: itemBuilder,
+      crossAxisCount: crossAxisCount,
+      mainAxisSpacing: mainAxisSpacing,
+      crossAxisSpacing: crossAxisSpacing,
+    );
