e9b9cda149255a40c9d5a057d358160920d201b5
jeisonmanuelvargasvargas <jeisonvargas@flutterlabapps.com>
2025-04-09 11:32:11 -0500

    fix(context_extension): implemented size

---
+++ lib/src/widgets/buttons/circular_button/circular_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:forms360_uikit/forms360_uikit.dart';
+
+class CircularButton extends StatelessWidget {
+  final Function() onTap;
+  final IconData icon;
+
+  const CircularButton({super.key, required this.onTap, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 10,
+      shape: const CircleBorder(),
+      color: context.primaryColor,
+      child: InkWell(
+        customBorder: const CircleBorder(),
+        onTap: onTap,
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Icon(icon, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
