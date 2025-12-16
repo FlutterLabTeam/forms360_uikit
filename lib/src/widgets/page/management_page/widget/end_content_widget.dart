8eacb7820e26cac8bdca9ef16afc73e6bfd8fe77
jeisonmanuelvargasvargas <jeisonvargas@flutterlabapps.com>
2025-12-16 17:48:16 -0500

    feedback

--- lib/src/widgets/page/management_page/widget/end_content_widget.dart
+++ lib/src/widgets/page/management_page/widget/end_content_widget.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forms360_uikit/forms360_uikit.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forms360_uikit/src/widgets/page/management_page/widget/error_retry_widget.dart';
 
 class EndContentWidget extends ConsumerStatefulWidget {
   final Function(String)? onSearch;
-  final String searchLabel;
-  final Widget content;
-  final Widget? titleWidget;
-  final String? retryButtonText;
   final bool isLoading;
+  final Widget content;
+  final String searchLabel;
+  final Widget? titleWidget;
   final String? errorMessage;
   final Function retryCallback;
+  final String? retryButtonText;
   final List<Widget>? settingsWidgets;
 
   const EndContentWidget(
       {super.key,
       this.onSearch,
-      this.searchLabel = 'Search',
-      this.retryButtonText = 'Retry',
       this.titleWidget,
       this.errorMessage,
-      this.isLoading = false,
       this.settingsWidgets,
       required this.content,
+      this.isLoading = false,
+      this.searchLabel = 'Search',
+      this.retryButtonText = 'Retry',
       required this.retryCallback});
 
   @override
